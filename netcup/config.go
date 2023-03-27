2d113db79fd803e1931bc09ba089336139885774
navythenerd <dev@navythenerd.com>
2023-03-27 15:36:54 +0200

    add config struct

--- netcup/config.go
+++ netcup/config.go
@@ -1,7 +1,17 @@
 package netcup
 
 type Config struct {
-	ApiKey         string
-	ApiPassword    string
-	CustomerNumber string
+	ApiKey         string `json:"apiKey"`
+	ApiPassword    string `json:"apiPassword"`
+	CustomerNumber string `json:"customerNumber"`
+}
+
+func ReadConfigFromFile(cfg *Config) error {
+
+	return nil
+}
+
+func ReadConfigFromEnv(cfg *Config) error {
+
+	return nil
 }
