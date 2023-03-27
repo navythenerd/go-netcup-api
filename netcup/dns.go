7f7d6231e1180584b195b0571e8256058381d745
navythenerd <dev@navythenerd.com>
2023-03-27 15:36:33 +0200

    update dnsrecord struct

--- netcup/dns.go
+++ netcup/dns.go
@@ -1,9 +1,9 @@
 package netcup
 
 type DNSRecord struct {
-	Hostname    string
-	Type        string
-	Destination string
-	DomainId    string
-	Priority    int
+	Id          string `json:"id,omitempty"`
+	Hostname    string `json:"hostname"`
+	Type        string `json:"type"`
+	Destination string `json:"destination"`
+	Priority    int    `json:"priority,omitempty"`
 }
